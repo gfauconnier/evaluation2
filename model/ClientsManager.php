7a4f6dd6bc46f625ae66ca08e0ccfda766633f9c
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-11-07 12:00:02 +0100

    added Hydrate trait in Hydrate.php, services.php in service folder. Added basic getters and setters to Clint class. Added clientExists and createClient methods in clientmanager

--- model/ClientsManager.php
+++ model/ClientsManager.php
@@ -16,17 +16,26 @@ class ClientsManager {
   }
 
   // METHODS
-// creates a client in db if it deosn't exist yet
+// creates a client in db if it doesn't exist yet
   public function createClient(Client $client)
   {
-
+    if (!$this->clientExists($client)) {
+      $query = $this->_db->prepare('INSERT INTO clients (f_name, l_name, user_name, password) VALUES (:f_name, :l_name, :user_name, :password)');
+      $query->bindValue(':f_name', $client->getF_name());
+      $query->bindValue(':l_name', $client->getL_name());
+      $query->bindValue(':user_name', $client->getUser_name());
+      $query->bindValue(':password', $client->getPassword());
+      $query->execute();
+      return 'Client created';
+    }
+    return 'This username is already in use';
   }
 
 // gets the client depending on sent id   possible trait
   public function getClient(Client $client)
   {
     if ($this->clientExists($client)) {
-      $query = $this->_db->query('SELECT * FROM clients WHERE id_client = '.$client->$id);
+      $query = $this->_db->query('SELECT * FROM clients WHERE user_name = '.$client->getUser_name());
       $data = $query->fetch(PDO::FETCH_ASSOC);
       $client->hydrate($data);
       return $client;
@@ -34,9 +43,21 @@ class ClientsManager {
     return false;
   }
 
+// verifies the user_name and password sent
+  public function connectUser(Client $client)
+  {
+
+  }
+
 // possible trait
   public function clientExists(Client $client)
   {
-
+    $query = $this->_db->query('SELECT * FROM clients WHERE user_name = \''.$client->getUser_name().'\'');
+    $data = $query->fetch(PDO::FETCH_ASSOC);
+    if ($data) {
+        return true;
+    } else {
+        return false;
+    }
   }
 }
