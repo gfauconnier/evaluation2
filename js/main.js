3853e5ad9f6298102e3bd73492b59f6649b771ab
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-11-10 09:22:40 +0100

    added cdn to popper in foot.php, made tabs work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-$('#accounts_table').stacktable();
+// stacktable
+$('#accounts_table').stacktable()
 
 // adds a link to tbody rows
 $('.accounts').click(function(){
   window.location.href = 'account_details.php?id_account=' + this.id;
-});
+})
+
+// bootstrap tabs display
+$('#accountTabs a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
--- view/account_details_v.php
+++ view/account_details_v.php
@@ -7,12 +7,24 @@ $account = $current_account->getAttributes();
 foreach ($account as $key => $value) {
   echo $key.' - '.$value.'<br>';
 }
-
-echo $deposit_form->getForm();
-echo $withdraw_form->getForm();
-echo $transfer_form->getForm();
-
 ?>
+<nav class="nav nav-tabs" id="accountTabs" role="tablist">
+  <a class="nav-item nav-link active" id="nav-deposit-tab" data-toggle="tab" href="#nav-deposit" role="tab" aria-controls="nav-deposit" aria-selected="true">Deposit</a>
+  <a class="nav-item nav-link" id="nav-withdraw-tab" data-toggle="tab" href="#nav-withdraw" role="tab" aria-controls="nav-withdraw" aria-selected="false">Withdrawal</a>
+  <a class="nav-item nav-link" id="nav-transfer-tab" data-toggle="tab" href="#nav-transfer" role="tab" aria-controls="nav-transfer" aria-selected="false">Transfer</a>
+</nav>
+
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-deposit" role="tabpanel" aria-labelledby="nav-deposit-tab">
+    <?php echo $deposit_form->getForm(); ?>
+  </div>
+  <div class="tab-pane fade" id="nav-withdraw" role="tabpanel" aria-labelledby="nav-withdraw-tab">
+    <?php echo $withdraw_form->getForm(); ?>
+  </div>
+  <div class="tab-pane fade" id="nav-transfer" role="tabpanel" aria-labelledby="nav-transfer-tab">
+    <?php echo $transfer_form->getForm(); ?>
+  </div>
+</div>
 
 
 
